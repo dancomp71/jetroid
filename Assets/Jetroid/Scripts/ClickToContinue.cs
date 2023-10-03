aa8514f47a39fa282bfd3a490f3201914449c0c2
Daniel Serrano <dancomp71@aol.com>
2023-10-03 08:51:24 -0400

    added scenes and create a splash screen and game start mode, at a basic level.

---
+++ Assets/Jetroid/Scripts/ClickToContinue.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickToContinue : MonoBehaviour
+{
+    public string scene;
+
+    private bool loadLock;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && !loadLock)
+        {
+            LoadScene();
+        }
+    }
+
+    private void LoadScene()
+    {
+        loadLock = true;
+        SceneManager.LoadScene(scene);
+    }
+}
